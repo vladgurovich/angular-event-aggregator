e163b065817c15d30baeded3b71bf76efeaac6dd
Vlad Gurovich <vlad@subatomicsystems.com>
2014-03-30 01:59:59 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-angular-observer
+angular-event-aggregator
 ================
 
-scope-independent AngularJS observer service
+scope-independent AngularJS Event Aggregator service inspired by Backbones event aggregator.
+
+## Usage
+
+Include `angular-event-aggregator.js` after your main AngularJS script.
+
+Specify `ngEventAggregator` as a dependency for your Angular application:
+
+```js
+var app = angular.module('myApp', ['ngEventAggregator']);
+```
+
+Now you can inject `eventAggregator` anywhere in your application, and register a listener.
+
+```js
+app.controller("MyCtrl", ['eventAggregator', function(eventAggregator){
+
+  var myCallback = function(item) {
+    console.log(item);
+    eventAggregator.off('some.event.happens', myCallBack);
+  };
+  eventAggregator.on('some.event.happens', myCallBack);
+}]);
+```
+or execute a trigger.
+```js
+app.controller("SomeOtherCtrl", ['eventAggregator', function(eventAggregator){
+  eventAggregator.trigger('some.event.happens', {data: 'payload'});
+}]);
+```
+
---
+++ angular-event-aggregator.js
@@ -0,0 +1,45 @@
+(function(window, angular, undefined) {
+  'use strict';
+   // declaring the module in one file / anonymous function
+   angular.module('ngEventAggregator', [])
+   .factory('eventAggregator', [ function () {
+    var _eventListeners;
+
+    function on(eventName, callback) {
+      if(angular.isUndefined(_eventListeners)) {
+        _eventListeners = {};
+      }
+      var eventListeners = _eventListeners[eventName] || (_eventListeners[name] = []);
+      eventListeners.push(callback);
+    }
+
+
+    function off(eventName, callback){
+      var eventListeners = _eventListeners[eventName];
+      if(angular.isUndefined(eventListeners)) {
+        return;
+      }
+      //iterating in reverse avoids problems when removing items
+      for (var i = eventListeners.length - 1; i >= 0; --i) {
+        if(eventListeners[i] === callback){
+          eventListeners.splice(i,1);
+        }
+      }
+    }
+
+    function trigger(eventName, eventObject) {
+      var eventListeners = _eventListeners[eventName];
+      if(angular.isDefined(eventListeners)) {
+        for (var i = 0; i < eventListenerslength - 1;  i++) {
+          _eventlisteners[eventName][i](eventObject);
+        }
+      }
+    }
+
+    return {
+      on: on,
+      off: off,
+      trigger: trigger
+    };
+   }]);
+})(window, window.angular);
